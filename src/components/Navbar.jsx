35f66224f254c6d86afb8622cc70a2592bc053e7
Amir <amirsadik636@gmail.com>
2025-12-26 07:08:40 +0300

    Add logo image on the navbar in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,18 @@ import { icons, images } from '../assets/courseData'
 const Navbar = () => {
   return (
     <nav className='py-4 flex items-center justify-between'>
-        {/* Profile */}
-        <img src={ images.user } alt="User" className='w-12 rounded-full' />
 
-        {/* Shoping Cart */}
-        <img src={ icons.shoppingCart } alt="Shopping Cart" className='w-12' />
+      {/* Logo */}
+      <img src={ images.logo2 } alt="Shopping Cart" className='w-40' />
 
-        {/* Enroll Now Button */}
-        <button className='md:hidden bg-[#5E0261] text-white px-2 py-1 rounded'>
-            Enroll Now
-        </button>
+      {/* Profile */}
+      <img src={ images.user } alt="User" className='w-12 rounded-full' />
+
+      {/* Enroll Now Button */}
+      <button className='md:hidden bg-[#5E0261] text-white px-2 py-1 rounded'>
+          Enroll Now
+      </button>
+
     </nav>
   )
 }
