458cd7829297da70af16b922e868b1682d2634fc
Amir <amirsadik636@gmail.com>
2025-12-26 09:54:44 +0300

    Create related courses section in RelatedCourses.jsx

---
+++ src/components/RelatedCourses.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Title from './Title'
+import { relatedCourses } from '../assets/courseData'
+import Rating from './Rating'
+
+const RelatedCourses = () => {
+  return (
+    <section className='my-20'>
+        <Title title='Related Courses You May Like' />
+
+        <div className='mt-10 flex items-start gap-10'>
+            {
+                relatedCourses.map((course, i) => (
+                    <div key={i} className='cursor-pointer border border-white p-2 rounded
+                    hover:border-gray-200 hover:shadow-xl transition-all duration-300'>
+                        <img src={ course.image } alt={`${ course.name } Course`}
+                        className='w-40' />
+                        <h2 className='max-w-40 text-sm font-bold'>{ course.name }</h2>
+                        <Rating/>
+                    </div>
+                ))
+            }
+        </div>
+    </section>
+  )
+}
+
+export default RelatedCourses
