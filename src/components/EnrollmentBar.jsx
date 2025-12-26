57dee3ce77f945ab6e1427a372ebaa02f2379b2d
Amir <amirsadik636@gmail.com>
2025-12-26 07:34:16 +0300

    Create the enrollment bar in EnrollmentBar.jsx

---
+++ src/components/EnrollmentBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { icons, images } from '../assets/courseData'
+import Preview from './Preview'
+import Rating from './Rating'
+import CourseData from './CourseData'
+import EnrollNow from './EnrollNow'
+
+const EnrollmentBar = () => {
+  return (
+    <section className='border border-gray-400 w-[360px] rounded-xl p-4 mt-4'>
+        <Preview/>
+        <Rating/>
+        <CourseData/>
+        <EnrollNow/>
+    </section>
+  )
+}
+
+export default EnrollmentBar
