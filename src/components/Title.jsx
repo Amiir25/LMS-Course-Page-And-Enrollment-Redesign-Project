3dcd69db2a918e8071d21faee84b30929aba7350
Amir <amirsadik636@gmail.com>
2025-12-26 10:05:05 +0300

    Create a separate Title component in Title.jsx

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({ title }) => {
+  return (
+    <h2 className='text-3xl font-semibold mb-4'>
+        { title }
+    </h2>
+  )
+}
+
+export default Title
