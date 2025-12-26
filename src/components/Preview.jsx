7b2b3a827594517e2116d51cbde1c3c36d55d8cb
Amir <amirsadik636@gmail.com>
2025-12-26 07:32:03 +0300

    Create the preview section of the enrollment bar in Preview.jsx

---
+++ src/components/Preview.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { icons, images } from '../assets/courseData'
+
+const Preview = () => {
+  return (
+    <section className='relative w-full'>
+        {/* Web Development Image */}
+        <img src={ images.webDevelopment } alt="Web Development Course"
+        className='w-full opacity-80 rounded' />
+
+        {/* Play Icon */}
+        <img src={ icons.playButton } alt="Play Button"
+        className='w-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer' />
+
+        <p className='absolute bottom-6 left-1/2 -translate-x-1/2 text-white text-lg'>
+            Preview this course
+        </p>
+    </section>
+  )
+}
+
+export default Preview
