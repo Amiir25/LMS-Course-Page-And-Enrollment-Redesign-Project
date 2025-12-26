bcc7ca0d61ae6308ab1696a9494c288d7f57d4f8
Amir <amirsadik636@gmail.com>
2025-12-26 17:56:00 +0300

    Add unique key value for lists

--- src/components/CourseObjective.jsx
+++ src/components/CourseObjective.jsx
@@ -4,14 +4,19 @@ import Title from './Title'
 
 const CourseObjective = () => {
   return (
-    <section className='mt-40'>
-        
+    <section className='relative mt-40 border-t border-gray-300 px-1 py-4 rounded-2x shadow-2xl'>
+
         <Title title="By the end of this course, you'll be able to:" />
+
+        {/*  */}
+        <div className='border border-gray-300 w-fit p-1 rounded-full absolute -top-3 left-1/2 -translate-x-1/2'>
+            <p className='w-4 h-4 bg-gray-300 rounded-full'></p>
+        </div>
 
         <div className='flex items-start gap-4'>
             {
-                courseObjectives.map((section) => (
-                    <div className=''>
+                courseObjectives.map((section, i) => (
+                    <div key={i} className=''>
                         {
                             section.map((objective, i) => (
                                 <div key={i} className='flex items-start gap-2 my-2'>
