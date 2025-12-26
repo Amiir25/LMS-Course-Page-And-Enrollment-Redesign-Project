f8675d76dea0f0905f6f38eed5adc9be62ac55cd
Amir <amirsadik636@gmail.com>
2025-12-26 08:59:11 +0300

    Create requirements section in Requirements.jsx

---
+++ src/components/Requirements.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { requirements } from '../assets/courseData'
+
+const Requirements = () => {
+  return (
+    <section className='mt-20'>
+        <h2 className='text-3xl font-semibold mb-4'>Requirements</h2>
+        <div>
+            {
+                requirements.map((requirement, i) => (
+                    <p>⦿ { requirement }</p>
+                ))
+            }
+        </div>
+    </section>
+  )
+}
+
+export default Requirements
