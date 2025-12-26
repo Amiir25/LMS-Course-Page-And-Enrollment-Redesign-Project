faf1ea230a9d10674478332207743e74e532e133
Amir <amirsadik636@gmail.com>
2025-12-26 07:33:43 +0300

    Create Enroll Now button in EnrollNow.jsx

---
+++ src/components/EnrollNow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const EnrollNow = () => {
+  return (
+    <section className='flex justify-center'>
+        <button className='mt-10 bg-[#5E0261] text-white text-5xl font-bold px-8 py-4 rounded-2xl
+        cursor-pointer hover:opacity-90 active:opacity-100' >
+            Enroll Now
+        </button>
+    </section>
+  )
+}
+
+export default EnrollNow
